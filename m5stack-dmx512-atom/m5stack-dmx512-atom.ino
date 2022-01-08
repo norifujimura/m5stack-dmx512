fc13d36b82beadca701516e34b9b0fbe08917ce7
Noriyuki Fujimura <nori_fujimura@me.com>
2022-01-08 14:50:32 +0900

    start working

--- m5stack-dmx512-atom/m5stack-dmx512-atom.ino
+++ m5stack-dmx512-atom/m5stack-dmx512-atom.ino
@@ -1,19 +1,23 @@
 
-#include <M5Core2.h>
+#include "M5Atom.h"
 #include <SoftwareSerial.h>
-#define swrxPin 32
-#define swtxPin 33
+#define swrxPin 26
+#define swtxPin 32
 
 SoftwareSerial mySerial;
 
 int brightness = 0;
 //int Tx = 33;//DMXoutputPin
 void setup() {
-  M5.begin();
+  M5.begin(true, false, true);
+  delay(50);
+  M5.dis.fillpix(0xAAAAAA); 
+  /*
   M5.Lcd.clear(); 
   M5.Lcd.setCursor(0, 0);
   M5.Lcd.setTextSize(1);
   M5.Lcd.print("M5Stack DMX512");
+  */
   //Serial2.begin(250000,SERIAL_8N2,32,33);//Grove
 }
 void loop() {
